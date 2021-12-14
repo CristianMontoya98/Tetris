dc7005ee42175f927e09abf1a8281095c7726f03
Christian Montoya <chrimonlo@gmail.com>
2021-12-13 23:03:38 -0500

    Pantalla de inicio agregada y puntajes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 "use strict";
 import { L, Li, Z, Zi, Sqare, T, l } from "./figure.js";
+let play = document.getElementById("play");
+let play2 = document.getElementById("play2");
+let message = document.getElementsByClassName("message")[0];
 /* definition of the variables to be 
 used with respect to the canvas */
 let canvas;
 let context;
-
+let puntos;
+let nivel;
+let lineas;
+let pts = 0;
+let nv = 0;
+let ln = 0;
+let inter = 500;
+let col = 0;
 // Size of the figures
 let widhtFigure = 35;
 let heightFigure = 35;
@@ -37,12 +47,23 @@ let scenario = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
+//Event listener to start the game
+play2.addEventListener('click', () => {
+    message.style.display = "none";
+    start();
+});
 
+//Event listener to start the game
+play.addEventListener('click', () => {
+    start(); 
+});
 //function to asign the background color in the positions of the matrix with value 0
 function paintScenario() {
-    let color;
+  let color;
     let fila = 0;
+
   for (let i = 0; i < scenario.length; i++) {
+      
     for (let j = 0; j < scenario[i].length; j++) {
       if (scenario[i][j] == 0) {
         color = background;
@@ -69,29 +90,25 @@ function paintScenario() {
           widhtFigure,
           heightFigure
         );
-          fila++;
-          console.log(fila);
-      }
-        else if (scenario[i][j] == 2) {
-            color = "blue";
-            context.fillStyle = color;
-            context.fillRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-            );
-            context.strokeStyle = "black";
-            context.strokeRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-          );
-          fila++;
-          console.log(fila);
-        }
-        else if (scenario[i][j] == 3) {
+        fila++;
+      } else if (scenario[i][j] == 2) {
+        color = "blue";
+        context.fillStyle = color;
+        context.fillRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        context.strokeStyle = "black";
+        context.strokeRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        fila++;
+      } else if (scenario[i][j] == 3) {
         color = "green";
         context.fillStyle = color;
         context.fillRect(
@@ -106,49 +123,43 @@ function paintScenario() {
           i * heightFigure,
           widhtFigure,
           heightFigure
-          );
-          fila++;
-          console.log(fila);
-        }
-        else if (scenario[i][j] == 4) {
-            color = "yellow";
-            context.fillStyle = color;
-            context.fillRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-            );
-            context.strokeStyle = "black";
-            context.strokeRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-          );
-          fila++;
-          console.log(fila);
-        }
-        else if (scenario[i][j] == 5) {
-            color = "pink";
-            context.fillStyle = color;
-            context.fillRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-            );
-            context.strokeStyle = "black";
-            context.strokeRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-          );
-          fila++;
-          console.log(fila);
-        }
-        else if (scenario[i][j] == 6) {
+        );
+        fila++;
+      } else if (scenario[i][j] == 4) {
+        color = "yellow";
+        context.fillStyle = color;
+        context.fillRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        context.strokeStyle = "black";
+        context.strokeRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        fila++;
+      } else if (scenario[i][j] == 5) {
+        color = "pink";
+        context.fillStyle = color;
+        context.fillRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        context.strokeStyle = "black";
+        context.strokeRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        fila++;
+      } else if (scenario[i][j] == 6) {
         color = "orange";
         context.fillStyle = color;
         context.fillRect(
@@ -163,69 +174,64 @@ function paintScenario() {
           i * heightFigure,
           widhtFigure,
           heightFigure
-          );
-          fila++;
-          console.log(fila);
+        );
+        fila++;
       } else if (scenario[i][j] == 7) {
-          
-            color = "brown";
-            context.fillStyle = color;
-            context.fillRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-            );
-            context.strokeStyle = "black";
-            context.strokeRect(
-              j * widhtFigure,
-              i * heightFigure,
-              widhtFigure,
-              heightFigure
-          );
-          fila++;
-          console.log(fila);
-            }
-        
+        color = "brown";
+        context.fillStyle = color;
+        context.fillRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        context.strokeStyle = "black";
+        context.strokeRect(
+          j * widhtFigure,
+          i * heightFigure,
+          widhtFigure,
+          heightFigure
+        );
+        fila++;
       }
-      
-      if (fila == scenario[i].length) {
-          scenario.splice(i, 1);
-          scenario.unshift([0,0,0,0,0,0,0,0,0,0]);
-      }
-      fila=0;
-      
+    }
+
+    if (fila == scenario[i].length) {
+      scenario.splice(i, 1);
+      scenario.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      ln++;
+      pts += 10;
+      validateLevel();
+    }
+    fila = 0;
   }
 }
 //Generates a random number and create a new object of the figure in each case
 function randomFigure() {
-    let numRandom = Math.floor(Math.random() * 7);
-    let figAux;
-    if (numRandom == 0) {
-        figAux = new L();
-    }
-    if (numRandom == 1) {
-        figAux = new Li();
-    }
-    if (numRandom == 2) {
-        figAux = new Z();
-    }
-    if (numRandom == 3) {
-        figAux = new Zi();
-    }
-    if (numRandom == 4) {
-        figAux = new Sqare();
-    }
-    if (numRandom == 5) {
-        figAux = new T();
-    }
-    if (numRandom == 6) {
-        figAux = new l();
-    }
-    return figAux;
-
-
-  
+  let numRandom = Math.floor(Math.random() * 7);
+  let figAux;
+  if (numRandom == 0) {
+    figAux = new L();
+  }
+  if (numRandom == 1) {
+    figAux = new Li();
+  }
+  if (numRandom == 2) {
+    figAux = new Z();
+  }
+  if (numRandom == 3) {
+    figAux = new Zi();
+  }
+  if (numRandom == 4) {
+    figAux = new Sqare();
+  }
+  if (numRandom == 5) {
+    figAux = new T();
+  }
+  if (numRandom == 6) {
+    figAux = new l();
+  }
+  return figAux;
 }
 
 //Function to paint the figure respect the coordinates of each one
@@ -285,15 +291,17 @@ function paintFigure(fig) {
 
 //moveDown function, move the figure down until it hits the ground or other figure
 function moveDown(fig) {
-  if (
-    (fig.coordinates[0][0] != 19 &&
+  
+  
+    if (
+    fig.coordinates[0][0] != 19 &&
     fig.coordinates[1][0] != 19 &&
     fig.coordinates[2][0] != 19 &&
-    fig.coordinates[3][0] != 19) &&
-    (scenario[fig.coordinates[3][0] + 1][fig.coordinates[3][1]] == 0 &&
+    fig.coordinates[3][0] != 19 &&
+    scenario[fig.coordinates[3][0] + 1][fig.coordinates[3][1]] == 0 &&
     scenario[fig.coordinates[2][0] + 1][fig.coordinates[2][1]] == 0 &&
     scenario[fig.coordinates[1][0] + 1][fig.coordinates[1][1]] == 0 &&
-    scenario[fig.coordinates[0][0] + 1][fig.coordinates[0][1]] == 0)
+    scenario[fig.coordinates[0][0] + 1][fig.coordinates[0][1]] == 0
   ) {
     fig.coordinates[0][0]++;
     fig.coordinates[1][0]++;
@@ -306,14 +314,14 @@ function moveDown(fig) {
 //Move the figure to the right when is inside the canvas size and is not other figure in the next coordinate
 function moveRight(fig) {
   if (
-    (fig.coordinates[0][1] != 9 &&
+    fig.coordinates[0][1] != 9 &&
     fig.coordinates[1][1] != 9 &&
     fig.coordinates[2][1] != 9 &&
-    fig.coordinates[3][1] != 9) &&
-    (scenario[fig.coordinates[3][0]][fig.coordinates[3][1] + 1] == 0 &&
-        scenario[fig.coordinates[2][0]][fig.coordinates[2][1] + 1] == 0&&
-        scenario[fig.coordinates[1][0]][fig.coordinates[1][1] + 1] == 0 &&
-        scenario[fig.coordinates[0][0]][fig.coordinates[0][1] + 1] == 0)
+    fig.coordinates[3][1] != 9 &&
+    scenario[fig.coordinates[3][0]][fig.coordinates[3][1] + 1] == 0 &&
+    scenario[fig.coordinates[2][0]][fig.coordinates[2][1] + 1] == 0 &&
+    scenario[fig.coordinates[1][0]][fig.coordinates[1][1] + 1] == 0 &&
+    scenario[fig.coordinates[0][0]][fig.coordinates[0][1] + 1] == 0
   ) {
     fig.coordinates[0][1]++;
     fig.coordinates[1][1]++;
@@ -325,14 +333,14 @@ function moveRight(fig) {
 //Move the figure to the left when is inside the canvas size and is not other figure in the next coordinate
 function moveLeft(fig) {
   if (
-    (fig.coordinates[0][1] != 0 &&
+    fig.coordinates[0][1] != 0 &&
     fig.coordinates[1][1] != 0 &&
     fig.coordinates[2][1] != 0 &&
-          fig.coordinates[3][1] != 0) &&
-          (scenario[fig.coordinates[3][0]][fig.coordinates[3][1] - 1] == 0 &&
-            scenario[fig.coordinates[2][0]][fig.coordinates[2][1] - 1] == 0&&
-            scenario[fig.coordinates[1][0]][fig.coordinates[1][1] - 1] == 0 &&
-            scenario[fig.coordinates[0][0]][fig.coordinates[0][1] - 1] == 0)
+    fig.coordinates[3][1] != 0 &&
+    scenario[fig.coordinates[3][0]][fig.coordinates[3][1] - 1] == 0 &&
+    scenario[fig.coordinates[2][0]][fig.coordinates[2][1] - 1] == 0 &&
+    scenario[fig.coordinates[1][0]][fig.coordinates[1][1] - 1] == 0 &&
+    scenario[fig.coordinates[0][0]][fig.coordinates[0][1] - 1] == 0
   ) {
     fig.coordinates[0][1]--;
     fig.coordinates[1][1]--;
@@ -344,22 +352,22 @@ function moveLeft(fig) {
 //Function to validate the key that is pressed and asign a movement
 function movements() {
   document.addEventListener("keydown", (push) => {
-    if (push.key == "ArrowRight") {
+    if (push.key == "d") {
       moveRight(figure);
       paintScenario();
       paintFigure(figure);
     }
-    if (push.key == "ArrowLeft") {
+    if (push.key == "a") {
       moveLeft(figure);
       paintScenario();
       paintFigure(figure);
     }
-    if (push.key == "ArrowDown") {
+    if (push.key == "s") {
       moveDown(figure);
       paintScenario();
       paintFigure(figure);
     }
-    if (push.key == "ArrowUp") {
+    if (push.key == "w") {
       paintScenario();
 
       figure.rotate();
@@ -373,44 +381,60 @@ function movements() {
 }
 //Function to fix the figure in the bottom or over other figure
 function fixedFigure(fig) {
-    let elemento;
-    if (fig.color == "red") {
-        elemento = 1;
-    }
-    if (fig.color == "blue") {
-        elemento = 2;
-    }
-    if (fig.color == "green") {
-        elemento = 3;
-    }
-    if (fig.color == "yellow") {
-        elemento = 4;
-    }
-    if (fig.color == "pink") {
-        elemento = 5;
-    }
-    if (fig.color == "orange") {
-        elemento = 6;
-    }
-    if (fig.color == "brown") {
-        elemento = 7;
-    }
+  let elemento;
+  if (fig.color == "red") {
+    elemento = 1;
+  }
+  if (fig.color == "blue") {
+    elemento = 2;
+  }
+  if (fig.color == "green") {
+    elemento = 3;
+  }
+  if (fig.color == "yellow") {
+    elemento = 4;
+  }
+  if (fig.color == "pink") {
+    elemento = 5;
+  }
+  if (fig.color == "orange") {
+    elemento = 6;
+  }
+  if (fig.color == "brown") {
+    elemento = 7;
+  }
   for (let i = 0; i < fig.coordinates.length; i++) {
     scenario[fig.coordinates[i][0]][fig.coordinates[i][1]] = elemento;
-    }
-    
+  }
+
   figure = randomFigure();
+}
+//Function to validate the increment of the level every 100 points 
+function validateLevel() {
+  if (pts % 100 == 0) {
+      nv++;
+      inter/=3;
+      console.log(inter);  
+  }
 }
 
 //starting function, initialize the canvas and context variables and execute other functions
 function start() {
+  puntos = document.getElementById("puntos");
+  nivel = document.getElementById("nivel");
+  lineas = document.getElementById("lineas");
   canvas = document.getElementById("canva");
   context = canvas.getContext("2d");
   movements();
+  
   setInterval(function () {
     paintScenario();
     paintFigure(figure);
     moveDown(figure);
-  }, 500);
+    puntos.innerText = pts;
+    lineas.innerText = ln;
+      
+    nivel.innerText = nv;
+  }, inter);
 }
-start();
+
